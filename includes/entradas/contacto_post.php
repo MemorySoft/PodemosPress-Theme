780890f58751c6dfb29155a8d7921de72e100369
Hector Asencio <cranca@gmail.com>
2016-05-12 18:22:51 +0200

    Renombrados los labels del CPT Contacto

--- includes/entradas/contacto_post.php
+++ includes/entradas/contacto_post.php
@@ -1,21 +1,34 @@
 <?php  
-function crear_direccion_de_contacto() {
-  register_post_type( 'direccion-contacto',
+/**
+*   CONTACTO POST TYPE
+*   -----------------
+*   Genera una ficha de contacto que incluye nombre,
+*   teléfono, email y canal de Telegram.
+*   
+*   Autor: Hector Asencio @MemorySoft
+*   Versión: 1.0
+*   @package PodemosPress
+*
+*/ 
+
+// POST TYPE
+function podemospress_crear_contacto() {
+  register_post_type( 'contacto',
     array(
       'labels' => array(
         'name' => 'Contactos',
-        'singular_name' => 'Dirección de contacto',
-        'add_new' => 'Añadir Dirección',
-        'add_new_item' => 'Nueva Dirección de contacto',
+        'singular_name' => 'Ficha de contacto',
+        'add_new' => 'Añadir ficha',
+        'add_new_item' => 'Nueva ficha de contacto',
         'edit' => 'Editar',
-        'edit_item' => 'Editar Dirección de contacto',
-        'new_item' => 'Nueva Dirección de contacto',
+        'edit_item' => 'Editar ficha de contacto',
+        'new_item' => 'Nueva ficha de contacto',
         'view' => 'Ver',
-        'view_item' => 'Ver Dirección de contacto',
-        'search_items' => 'Buscar Dirección de contacto',
-        'not_found' => 'Ninguna Dirección de contacto encontrado',
-        'not_found_in_trash' => 'Ninguna Dirección de contacto encontrado en la Papelera',
-        'parent' => 'Dirección de contacto superior'
+        'view_item' => 'Ver ficha de contacto',
+        'search_items' => 'Buscar ficha de contacto',
+        'not_found' => 'Ninguna ficha de contacto encontrado',
+        'not_found_in_trash' => 'Ninguna ficha de contacto encontrado en la Papelera',
+        'parent' => 'Ficha de contacto superior'
       ),
       'public' => true,
       'menu_position' => 50,
@@ -26,114 +39,101 @@ function crear_direccion_de_contacto() {
     )
   );
 }
-add_action( 'init', 'crear_direccion_de_contacto' );
-/*
-  Meta Boxes
- */
-// Registro de campos
-function direccion_de_contacto_register_meta_fields() {
-  register_meta( 
-    'direccion-contacto', 
-    'direccion_de_contacto_name', 
-    'sanitize_text_field', 
-    'direccion_de_contacto_custom_fields_auth_callback' 
-  ); 
-  register_meta( 
-    'direccion-contacto', 
-    'direccion_de_contacto_email', 
-    'sanitize_text_field', 
-    'direccion_de_contacto_custom_fields_auth_callback' 
-  );
-  register_meta( 
-    'direccion-contacto', 
-    'direccion_de_contacto_telegram_id', 
-    'sanitize_text_field', 
-    'direccion_de_contacto_custom_fields_auth_callback' 
-  );
-  register_meta( 
-    'direccion-contacto', 
-    'direccion_de_contacto_telegram_url', 
-    'sanitize_text_field', 
-    'direccion_de_contacto_custom_fields_auth_callback' 
-  );
-}
-// Comprobación de autorización
-function direccion_de_contacto_custom_fields_auth_callback ( $allowed, $meta_key, $post_id, $user_id, $cap, $caps ){
-  if ( 'direccion-contacto' == get_post_type( $post_id ) && current_user_can( 'edit_post', $post_id ) ) 
-   { $allowed = true; } else  { $allowed = false; } return $allowed;
-}
-// Constructor del Meta Box
-function direccion_de_contacto_meta_boxes() {
-  add_meta_box( 
-    'direccion_de_contacto-meta-box', 
-    __('Dirección de contacto'), 
-    'direccion_de_contacto_meta_box_callback', 
-    'direccion-contacto' 
-  );
-}
-// Output del Meta Box
-function direccion_de_contacto_meta_box_callback( $post ) {
-  // Nombre
-  ?>
-  <p>
-    <label class="label" for="direccion_de_contacto_name">
-      <?php _e("Nombre del cargo o área", 'podemospress'); ?>
-    </label><br>
-    <input  name="direccion_de_contacto_name" id="direccion_de_contacto_name" type="text" size="40" value="">
-    <span class="description">Ej: Secretaría de Comunicación</span>
-  </p>
-  <?php
-  // Email
-  ?>
-  <p>
-    <label class="label" for="direccion_de_contacto_email">
-      <?php _e("Email de contacto", 'podemospress'); ?>
-    </label><br>
-    <input  name="direccion_de_contacto_email" id="direccion_de_contacto_email" type="text" size="40" value="">
-  </p>
-  <?php
-  // Telegram
-  ?>
-  <p>
-    <label class="label" for="direccion_de_contacto_telegram_id">
-      <?php _e("Canal público de Telegram", 'podemospress'); ?>
-    </label><br>
-    <input  name="direccion_de_contacto_telegram_id" id="direccion_de_contacto_telegram_id" type="email" size="40" value="" placeholder="Ej: @PodemBalears">
-    <span class="description">ID de Telegram</span>
-    <br>
-    <input  name="direccion_de_contacto_telegram_url" id="direccion_de_contacto_telegram_url" type="text" size="40" value="" placeholder="https://telegram.me/PodemBalears">
-    <span class="description">Enlace del canal público</span>
-  </p>
-  <?php
-}
-// Guardar datos
-function guardar_direccion_de_contacto( $post_id, $post ){
-  if ( ! isset( $_POST['direccion_de_contacto_meta_box_noncename'] ) || ! wp_verify_nonce( $_POST['direccion_de_contacto_meta_box_noncename'], 'direccion_de_contacto_meta_box' ) ) {
-      return;
-  }
+add_action( 'init', 'podemospress_crear_contacto' );
 
-  if( isset( $_POST['direccion_de_contacto_name'] ) && $_POST['direccion_de_contacto_name'] != "" ) {
-      update_post_meta( $post_id, 'direccion_de_contacto_name', $_POST['direccion_de_contacto_name'] );
-  } else {
-      delete_post_meta( $post_id, 'direccion_de_contacto_name' );
+// META BOXES
+function podemospress_contacto_meta_box() {
+    add_meta_box( 'miembros-datos',
+        'Datos de contacto',
+        'podemospress_muestra_contacto_meta_box',
+        'contacto', 
+        'normal', 
+        'default'
+    );
+}
+add_action( 'admin_init', 'podemospress_contacto_meta_box' );
+
+function podemospress_muestra_contacto_meta_box( $contacto ) {
+  $telefono = esc_html( get_post_meta( $contacto->ID, 'contacto_telefono', true ) );
+  $email = esc_html( get_post_meta( $contacto->ID, 'contacto_email', true ) );
+  $telegram = esc_html( get_post_meta( $contacto->ID, 'contacto_telegram', true ) );
+  ?>
+  <table class="form-table">
+    <tr valign="top">
+      <th scope="row"></th>
+      <td>
+        <p>El título de este item corresponde al nombre del área o cargo. Todos los campos son opcionales.</p>
+        <hr>
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row">Teléfono</th>
+      <td>
+        <input 
+        type="tel" 
+        size="40" 
+        name="contacto_telefono" 
+        value="<?php echo $telefono; ?>" /><br>
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row">Email</th>
+      <td>
+        <input 
+        type="text" 
+        size="40" 
+        name="contacto_email" 
+        value="<?php echo $email; ?>" /><br>
+      </td>
+    </tr>
+    <tr valign="top">
+      <th scope="row">Telegram</th>
+      <td>
+        <input 
+        type="text" 
+        size="40" 
+        name="contacto_telegram" 
+        value="<?php echo $telegram; ?>" /><br>
+      </td>
+    </tr>
+  </table>
+  <?php
+}
+
+function podemospress_agrega_contacto_valores( $datos_id, $contacto ) {
+  if ( $contacto->post_type == 'contacto' ) {
+    if ( isset( $_POST['contacto_telefono'] ) && $_POST['contacto_telefono'] != '' ) {
+      update_post_meta( 
+        $datos_id, 
+        'contacto_telefono', 
+        $_POST['contacto_telefono'] 
+        );
+    } else {
+      delete_post_meta( $datos_id, 'contacto_telefono' );
+    }
   }
-  if( isset( $_POST['direccion_de_contacto_email'] ) && $_POST['direccion_de_contacto_email'] != "" ) {
-      update_post_meta( $post_id, 'direccion_de_contacto_email', $_POST['direccion_de_contacto_email'] );
-  } else {
-      delete_post_meta( $post_id, 'direccion_de_contacto_email' );
+  if ( $contacto->post_type == 'contacto' ) {
+    if ( isset( $_POST['contacto_email'] ) && $_POST['contacto_email'] != '' ) {
+      update_post_meta( 
+        $datos_id, 
+        'contacto_email', 
+        $_POST['contacto_email'] 
+        );
+    } else {
+      delete_post_meta( $datos_id, 'contacto_email' );
+    }
   }
-  if( isset( $_POST['direccion_de_contacto_telegram_id'] ) && $_POST['direccion_de_contacto_telegram_id'] != "" ) {
-      update_post_meta( $post_id, 'direccion_de_contacto_telegram_id', $_POST['direccion_de_contacto_telegram_id'] );
-  } else {
-      delete_post_meta( $post_id, 'direccion_de_contacto_telegram_id' );
-  }
-  if( isset( $_POST['direccion_de_contacto_telegram_url'] ) && $_POST['direccion_de_contacto_telegram_url'] != "" ) {
-      update_post_meta( $post_id, 'direccion_de_contacto_telegram_url', $_POST['direccion_de_contacto_telegram_url'] );
-  } else {
-      delete_post_meta( $post_id, 'direccion_de_contacto_telegram_url' );
+  if ( $contacto->post_type == 'contacto-item' ) {
+    if ( isset( $_POST['contacto_telegram'] ) && $_POST['contacto_telegram'] != '' ) {
+      update_post_meta( 
+        $datos_id, 
+        'contacto_telegram', 
+        $_POST['contacto_telegram'] 
+        );
+    } else {
+      delete_post_meta( $datos_id, 'contacto_telegram' );
+    }
   }
 }
-add_action( 'save_post', 'guardar_direccion_de_contacto', 10, 2 );
-add_action( 'init', 'direccion_de_contacto_register_meta_fields' );
-add_action('add_meta_boxes', 'direccion_de_contacto_meta_boxes');
+add_action( 'save_post', 'podemospress_agrega_contacto_valores', 10, 2 );
 ?>
