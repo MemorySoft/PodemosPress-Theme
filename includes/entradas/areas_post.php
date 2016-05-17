8677253fbc88bee1514832dce856930fad22cdb2
Hector Asencio <cranca@gmail.com>
2016-05-17 13:32:53 +0200

    Refactorización
    Se ha eleiminado el metabox que permitia insertar un enlace de contacto.

--- includes/entradas/areas_post.php
+++ includes/entradas/areas_post.php
@@ -10,6 +10,7 @@
 *   @package PodemosPress
 *
 */ 
+error_reporting(E_ALL ^ E_NOTICE);
 
 // POST TYPE
 function podemospress_crear_areas() {
@@ -40,40 +41,4 @@ function podemospress_crear_areas() {
   );
 }
 add_action( 'init', 'podemospress_crear_areas' );
-
-// METABOX
-function podemospress_area_enlace_meta_box() {
-    add_meta_box( 'area-enlace',
-        'Enlace opcional',
-        'podemospress_muestra_area_enlace_meta_box',
-        'area', 
-        'normal', 
-        'default'
-    );
-}
-add_action( 'admin_init', 'podemospress_area_enlace_meta_box' );
-
-function podemospress_muestra_area_enlace_meta_box() {
-  global $post;
-  $custom = get_post_custom($post->ID);
-  $enlace_contacto = $custom["area_contacto_visibilidad"][0]; 
-?>
-  <input 
-    type="checkbox" 
-    name="area_contacto_visibilidad" 
-    <?php if( $enlace_contacto == true ) { ?>checked="checked"<?php } ?> /> Selcciona para mostrar un enlace a la página de contacto.
-  <?php
-}
-
-function podemospress_guarda_checkbox($post_ID = 0) {
-  $post_ID = (int) $post_ID;
-  $post_type = get_post_type( $post_ID );
-  $post_status = get_post_status( $post_ID );
-
-  if ($post_type) {
-  update_post_meta($post_ID, "area_contacto_visibilidad", $_POST["area_contacto_visibilidad"]);
-  }
-  return $post_ID;
-}
-add_action('save_post', 'podemospress_guarda_checkbox');
 ?>
